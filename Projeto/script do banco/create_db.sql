5b0b4a520705e93fc34743ebda2989def13b6bad
Pedro Magalhães Bernardo <pedromagalhaesbernardo@gmail.com>
2015-10-12 10:57:48 -0300

    update database script

--- Projeto/script do banco/create_db.sql
+++ Projeto/script do banco/create_db.sql
@@ -5,282 +5,278 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema estanteUniversitaria
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema estanteuniversitaria
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema estanteUniversitaria
+-- Schema estanteuniversitaria
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `estanteUniversitaria` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `estanteUniversitaria` ;
+CREATE SCHEMA IF NOT EXISTS `estanteuniversitaria` DEFAULT CHARACTER SET utf8 ;
+USE `estanteuniversitaria` ;
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Usuario`
+-- Table `estanteuniversitaria`.`usuario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Usuario` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`usuario` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Usuario` (
-  `idUsuario` INT NOT NULL  AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`usuario` (
+  `idUsuario` INT(11) NOT NULL AUTO_INCREMENT,
   `email` TEXT NOT NULL,
   `password` TEXT NOT NULL,
-  `cep` TEXT NULL,
-  `endereco` TEXT NULL,
-  `endNum` INT NULL,
-  `endComplemento` TEXT NULL,
-  `estado` TEXT NULL,
-  `cidade` TEXT NULL,
-  `universidade` TEXT NULL,
-  `telefone` TEXT NULL,
-  `celular` TEXT NULL,
+  `cep` TEXT NULL DEFAULT NULL,
+  `endereco` TEXT NULL DEFAULT NULL,
+  `endNum` INT(11) NULL DEFAULT NULL,
+  `endComplemento` TEXT NULL DEFAULT NULL,
+  `estado` TEXT NULL DEFAULT NULL,
+  `cidade` TEXT NULL DEFAULT NULL,
+  `universidade` TEXT NULL DEFAULT NULL,
+  `telefone` TEXT NULL DEFAULT NULL,
+  `celular` TEXT NULL DEFAULT NULL,
   `nome` TEXT NOT NULL,
   PRIMARY KEY (`idUsuario`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Livro`
+-- Table `estanteuniversitaria`.`livro`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Livro` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`livro` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Livro` (
-  `idLivro`  INT NOT NULL AUTO_INCREMENT ,
-  `nome` TEXT NULL,
-  `autor` TEXT NULL,
-  `editora` TEXT NULL,
-  `edicao` TEXT NULL,
-  `ano` INT NULL,
-  `condicao` TEXT NULL,
-  PRIMARY KEY (`idLivro`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`livro` (
+  `idLivro` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` TEXT NULL DEFAULT NULL,
+  `autor` TEXT NULL DEFAULT NULL,
+  `editora` TEXT NULL DEFAULT NULL,
+  `edicao` TEXT NULL DEFAULT NULL,
+  `ano` INT(11) NULL DEFAULT NULL,
+  `condicao` TEXT NULL DEFAULT NULL,
+  `wishlist` TINYINT(1) NULL DEFAULT NULL,
+  `idUsuario` INT(11) NOT NULL,
+  `imagem` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`idLivro`),
+  INDEX `fk_idUsuario_livro` (`idUsuario` ASC),
+  CONSTRAINT `fk_idUsuario_livro`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 33
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Anuncio`
+-- Table `estanteuniversitaria`.`anuncio`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Anuncio` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`anuncio` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Anuncio` (
-  `idAnuncio`  INT NOT NULL AUTO_INCREMENT,
-  `idLivro` INT NULL,
-  `tipo` TEXT NULL,
-  `valor` DOUBLE NULL,
-  `idUsuario` INT NULL,
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`anuncio` (
+  `idAnuncio` INT(11) NOT NULL AUTO_INCREMENT,
+  `idLivro` INT(11) NULL DEFAULT NULL,
+  `tipo` TEXT NULL DEFAULT NULL,
+  `valor` DOUBLE NULL DEFAULT NULL,
+  `idUsuario` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idAnuncio`),
   INDEX `fk_usuario_idx` (`idUsuario` ASC),
   INDEX `fk_livro_idx` (`idLivro` ASC),
   CONSTRAINT `fk_livro_anuncio`
     FOREIGN KEY (`idLivro`)
-    REFERENCES `estanteUniversitaria`.`Livro` (`idLivro`)
+    REFERENCES `estanteuniversitaria`.`livro` (`idLivro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_anuncio`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Transacao`
+-- Table `estanteuniversitaria`.`transacao`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Transacao` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`transacao` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Transacao` (
-  `idTransacao` INT NOT NULL AUTO_INCREMENT,
-  `idUsuarioAnunciante` INT NULL,
-  `idUsuarioCliente` INT NULL,
-  `status` INT NULL,
-  `idAnuncio` INT NULL,
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`transacao` (
+  `idTransacao` INT(11) NOT NULL AUTO_INCREMENT,
+  `idUsuarioAnunciante` INT(11) NULL DEFAULT NULL,
+  `idUsuarioCliente` INT(11) NULL DEFAULT NULL,
+  `status` INT(11) NULL DEFAULT NULL,
+  `idAnuncio` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idTransacao`),
   INDEX `id_usuario_anunciante_idx` (`idUsuarioAnunciante` ASC),
   INDEX `fk_usuario_cliente_idx` (`idUsuarioCliente` ASC),
   INDEX `fk_anuncio_idx` (`idAnuncio` ASC),
+  CONSTRAINT `fk_anuncio_anunciante`
+    FOREIGN KEY (`idAnuncio`)
+    REFERENCES `estanteuniversitaria`.`anuncio` (`idAnuncio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_anunciante`
     FOREIGN KEY (`idUsuarioAnunciante`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_cliente`
     FOREIGN KEY (`idUsuarioCliente`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_anuncio_anunciante`
-    FOREIGN KEY (`idAnuncio`)
-    REFERENCES `estanteUniversitaria`.`Anuncio` (`idAnuncio`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Avaliacao`
+-- Table `estanteuniversitaria`.`avaliacao`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Avaliacao` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`avaliacao` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Avaliacao` (
-  `idAvaliacao` INT NOT NULL AUTO_INCREMENT,
-  `idUsuarioAvaliado` INT NULL,
-  `idUsuarioAvaliador` INT NULL,
-  `nota` INT NULL,
-  `review` TEXT NULL,
-  `idTransacao` INT NULL,
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`avaliacao` (
+  `idAvaliacao` INT(11) NOT NULL AUTO_INCREMENT,
+  `idUsuarioAvaliado` INT(11) NULL DEFAULT NULL,
+  `idUsuarioAvaliador` INT(11) NULL DEFAULT NULL,
+  `nota` INT(11) NULL DEFAULT NULL,
+  `review` TEXT NULL DEFAULT NULL,
+  `idTransacao` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idAvaliacao`),
   INDEX `fk_usuario_avaliado_idx` (`idUsuarioAvaliado` ASC),
   INDEX `fk_usuario_avaliador_idx` (`idUsuarioAvaliador` ASC),
   INDEX `fk_transacao_idx` (`idTransacao` ASC),
+  CONSTRAINT `fk_transacao_avaliacao`
+    FOREIGN KEY (`idTransacao`)
+    REFERENCES `estanteuniversitaria`.`transacao` (`idTransacao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_avaliado`
     FOREIGN KEY (`idUsuarioAvaliado`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_avaliador`
     FOREIGN KEY (`idUsuarioAvaliador`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transacao_avaliacao`
-    FOREIGN KEY (`idTransacao`)
-    REFERENCES `estanteUniversitaria`.`Transacao` (`idTransacao`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Wishlist`
+-- Table `estanteuniversitaria`.`chat`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Wishlist` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`chat` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Wishlist` (
-  `idWishlist` INT NOT NULL AUTO_INCREMENT,
-  `idLivro` INT NULL,
-  `idUsuario` INT NULL,
-  PRIMARY KEY (`idWishlist`),
-  INDEX `fk_livro_idx` (`idLivro` ASC),
-  INDEX `fk_usuario_idx` (`idUsuario` ASC),
-  CONSTRAINT `fk_livro_w`
-    FOREIGN KEY (`idLivro`)
-    REFERENCES `estanteUniversitaria`.`Livro` (`idLivro`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_usuario_w`
-    FOREIGN KEY (`idUsuario`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Favoritos`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Favoritos` ;
-
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Favoritos` (
-  `idFavorito` INT NOT NULL AUTO_INCREMENT,
-  `idUsuario` INT NULL,
-  `idAnuncio` INT NULL,
-  PRIMARY KEY (`idFavorito`),
-  INDEX `fk_usuario_idx` (`idUsuario` ASC),
-  INDEX `fk_anuncio_idx` (`idAnuncio` ASC),
-  CONSTRAINT `fk_usuario_favoritos`
-    FOREIGN KEY (`idUsuario`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_anuncio_favoritos`
-    FOREIGN KEY (`idAnuncio`)
-    REFERENCES `estanteUniversitaria`.`Anuncio` (`idAnuncio`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Comentario`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Comentario` ;
-
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Comentario` (
-  `idComentario` INT NOT NULL AUTO_INCREMENT,
-  `idUsuario` INT NULL,
-  `idAnuncio` INT NULL,
-  `texto` TEXT NULL,
-  `data` DATE NULL,
-  PRIMARY KEY (`idComentario`),
-  INDEX `fk_usuario_idx` (`idUsuario` ASC),
-  INDEX `fk_anuncio_idx` (`idAnuncio` ASC),
-  CONSTRAINT `fk_usuario_comentario`
-    FOREIGN KEY (`idUsuario`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_anuncio_comentario`
-    FOREIGN KEY (`idAnuncio`)
-    REFERENCES `estanteUniversitaria`.`Anuncio` (`idAnuncio`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Chat`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Chat` ;
-
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Chat` (
-  `idChat` INT NOT NULL AUTO_INCREMENT,
-  `idUsuario1` INT NULL,
-  `idUsuario2` INT NULL,
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`chat` (
+  `idChat` INT(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario1` INT(11) NULL DEFAULT NULL,
+  `idUsuario2` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idChat`),
   INDEX `fk_usuario1_idx` (`idUsuario1` ASC),
   INDEX `fk_usuario2_idx` (`idUsuario2` ASC),
   CONSTRAINT `fk_usuario1_chat`
     FOREIGN KEY (`idUsuario1`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario2_chat`
     FOREIGN KEY (`idUsuario2`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `estanteUniversitaria`.`Mensagem`
+-- Table `estanteuniversitaria`.`comentario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `estanteUniversitaria`.`Mensagem` ;
+DROP TABLE IF EXISTS `estanteuniversitaria`.`comentario` ;
 
-CREATE TABLE IF NOT EXISTS `estanteUniversitaria`.`Mensagem` (
-  `idMensagem` INT NOT NULL AUTO_INCREMENT,
-  `idChat` INT NULL,
-  `idUsuarioEnvia` INT NULL,
-  `idUsuarioRecebe` INT NULL,
-  `mensagem` TEXT NULL,
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`comentario` (
+  `idComentario` INT(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` INT(11) NULL DEFAULT NULL,
+  `idAnuncio` INT(11) NULL DEFAULT NULL,
+  `texto` TEXT NULL DEFAULT NULL,
+  `data` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`idComentario`),
+  INDEX `fk_usuario_idx` (`idUsuario` ASC),
+  INDEX `fk_anuncio_idx` (`idAnuncio` ASC),
+  CONSTRAINT `fk_anuncio_comentario`
+    FOREIGN KEY (`idAnuncio`)
+    REFERENCES `estanteuniversitaria`.`anuncio` (`idAnuncio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuario_comentario`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `estanteuniversitaria`.`favoritos`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `estanteuniversitaria`.`favoritos` ;
+
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`favoritos` (
+  `idFavorito` INT(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` INT(11) NULL DEFAULT NULL,
+  `idAnuncio` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`idFavorito`),
+  INDEX `fk_usuario_idx` (`idUsuario` ASC),
+  INDEX `fk_anuncio_idx` (`idAnuncio` ASC),
+  CONSTRAINT `fk_anuncio_favoritos`
+    FOREIGN KEY (`idAnuncio`)
+    REFERENCES `estanteuniversitaria`.`anuncio` (`idAnuncio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuario_favoritos`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `estanteuniversitaria`.`mensagem`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `estanteuniversitaria`.`mensagem` ;
+
+CREATE TABLE IF NOT EXISTS `estanteuniversitaria`.`mensagem` (
+  `idMensagem` INT(11) NOT NULL AUTO_INCREMENT,
+  `idChat` INT(11) NULL DEFAULT NULL,
+  `idUsuarioEnvia` INT(11) NULL DEFAULT NULL,
+  `idUsuarioRecebe` INT(11) NULL DEFAULT NULL,
+  `mensagem` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`idMensagem`),
   INDEX `fk_usuario_envia_idx` (`idUsuarioEnvia` ASC),
   INDEX `fk_usuario_recebe_idx` (`idUsuarioRecebe` ASC),
   INDEX `fk_chat_idx` (`idChat` ASC),
   CONSTRAINT `fk_chat_mensagem`
     FOREIGN KEY (`idChat`)
-    REFERENCES `estanteUniversitaria`.`Chat` (`idChat`)
+    REFERENCES `estanteuniversitaria`.`chat` (`idChat`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_envia`
     FOREIGN KEY (`idUsuarioEnvia`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_usuario_recebe`
     FOREIGN KEY (`idUsuarioRecebe`)
-    REFERENCES `estanteUniversitaria`.`Usuario` (`idUsuario`)
+    REFERENCES `estanteuniversitaria`.`usuario` (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
